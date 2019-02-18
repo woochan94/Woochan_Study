d486e76673fefa7ef0e031d48a2a96dface73c1f
정우찬 <vdncks25@naver.com>
2019-02-18 20:19:04 +0900

    VanillaJS - ToDoList 강의 완료

---
+++ Nomad_edu/VanillaJS(JavaScript)/code/gretting.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) {
+        askForName();
+    }else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
---
+++ Nomad_edu/VanillaJS(JavaScript)/code/todo.js
@@ -0,0 +1,70 @@
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+let toDos = []; 
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    // 자바 스크립트는 local storage에 있는 모든 데이터를 string으로 저장함. 
+    // 그러므로 toDos에 들어있는 Object를 string으로 변환시켜서 local storage에 넣어야 함 
+    // 그 방법으로 JSON.stringify를 사용함 
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "\u{274C}";
+    delBtn.addEventListener("click", deleteToDo);
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
