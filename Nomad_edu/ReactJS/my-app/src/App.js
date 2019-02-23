69cd146004802b1935753a1b3bda88e6cf3be662
정우찬 <vdncks25@naver.com>
2019-02-23 18:51:13 +0900

    NomadCoder_ReactJS #4-2 실습
    setState를 이용해서 새로운 state 추가해봄

--- Nomad_edu/ReactJS/my-app/src/App.js
+++ Nomad_edu/ReactJS/my-app/src/App.js
@@ -2,43 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    id : 1,
-    title : "Matrix",
-    poster : "http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg"
-  },
-  {
-    id : 2,
-    title : "Full Metal Jacket",
-    poster : "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F"
-  },
-  {
-    id : 3,
-    title : "Oldboy",
-    poster : "http://artinsight.co.kr/n_news/peg/1508/thumb/a4cb4757610239a28cf0cba125b24a69_vpZESkDO68sOb6AbZkW9hFd.jpg"
-  },
-  {
-    id : 4,
-    title : "Star Wars",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-  }
-]
-
 class App extends Component {
 
-  componentWillMount() {
-    console.log('will mount');
+  state = {
+    greeting : 'Hello',
+    movies : [
+      {
+        id : 1,
+        title : "Matrix",
+        poster : "http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg"
+      },
+      {
+        id : 2,
+        title : "Full Metal Jacket",
+        poster : "https://img1.daumcdn.net/thumb/R338x491/?fname=http://t1.daumcdn.net/cfile/205CF91249EC79F00F"
+      },
+      {
+        id : 3,
+        title : "Oldboy",
+        poster : "http://artinsight.co.kr/n_news/peg/1508/thumb/a4cb4757610239a28cf0cba125b24a69_vpZESkDO68sOb6AbZkW9hFd.jpg"
+      },
+      {
+        id : 4,
+        title : "Star Wars",
+        poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
+      }
+    ]
   }
 
   componentDidMount() {
-    console.log('did mount');
+    setTimeout(() => {
+      this.setState({
+        movies : [
+          ...this.state.movies,
+          {
+            title: "Trainspotting",
+            poster :   "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
+          }
+        ]
+      }) 
+    },3000)
   }
+
   render() {
-    console.log('did render')
     return (
       <div className="App">
-          {movies.map((movie, index) => {
+        {this.state.greeting}
+          {this.state.movies.map((movie, index) => {
             return <Movie title={movie.title} poster={movie.poster} key={index} />
           })}
       </div>
