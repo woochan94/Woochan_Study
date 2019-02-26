de53eb642284e4ae91d556263b10f4e30dcc12c0
정우찬 <vdncks25@naver.com>
2019-02-26 21:25:11 +0900

    velopert Input 다루기
    PhoneForm에서 입력한 input값을 submit함수를 통해서 부모 컴포넌트인 App.js에 전송하고 받기

--- Nomad_edu/ReactJS/phone-book/src/App.js
+++ Nomad_edu/ReactJS/phone-book/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
   render() {
     return (
       <div>
-        <PhoneForm />
+        <PhoneForm onCreate={this.handleCreate} />
       </div>
     );
   }
--- Nomad_edu/ReactJS/phone-book/src/components/PhoneForm.js
+++ Nomad_edu/ReactJS/phone-book/src/components/PhoneForm.js
@@ -4,22 +4,36 @@ class PhoneForm extends Component {
 
     state = {
         name: '',
+        phone: ''
     }
 
 
+    // input의 값이 여러개 일 때는 input의 name속성을 사용한다. 
+    // name 값은 event.target.name을 통해서 조회할 수 있다. 
     handleChange = (e) => {
-        console.log(e);
         this.setState({
-            name: e.target.value    // 이벤트 객체에 담겨있는 현재의 텍스트 값을 가져올 수 있다. 
+            [e.target.name] : e.target.value 
+            // name: e.target.value    // 이벤트 객체에 담겨있는 현재의 텍스트 값을 가져올 수 있다. 
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); // 페이지 리로딩 방지 
+        this.props.onCreate(this.state); // 상태값을 onCreate를 통하여 부모에게 전달 
+        // 상태 초기화 
+        this.setState({
+            name : '',
+            phone : ''
         })
     }
 
     render() {
         return (
             <div>
-                <form>
-                    <input placeholder="이름" value={this.state.name} onChange={this.handleChange} />  
-                    <div>{this.state.name}</div>
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="이름" value={this.state.name} onChange={this.handleChange} name ="name" />  
+                    <input placeholder="전화번호" value={this.state.phone} onChange={this.handleChange} name = "phone" />
+                    <button type="submit">등록</button>
                 </form>
             </div>
         );
