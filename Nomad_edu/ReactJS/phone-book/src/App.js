08873b633546c0a702fc993015930a5cef5c7b07
정우찬 <vdncks25@naver.com>
2019-02-26 22:03:18 +0900

    velopert 전화번호 추가 기능

--- Nomad_edu/ReactJS/phone-book/src/App.js
+++ Nomad_edu/ReactJS/phone-book/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2 
+  state = {
+    information : [
+      {
+        id : 0,
+        name : '정우찬',
+        phone : '010-0000-0000'
+      }, 
+      {
+        id : 1,
+        name : '우찬쓰',
+        phone : '010-1111-1111'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state; 
+    this.setState({
+      information : information.concat({ id: this.id++, ...data })
+    })
   }
   render() {
+
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data = {this.state.information} />
       </div>
     );
   }
